7777782dcd975860a53c9ee15d218929b1a60fd3
Zakaria ElQotbi <zakaria.elqotbi@harman.com>
2016-09-09 14:11:00 +0200

    android: put android in immersive
    this try to put the application in a real fullscreen mode
    without the systemui bars.
    
    Signed-off-by: Zakaria ElQotbi <zakaria.elqotbi@harman.com>

--- android/src/org/linaro/glmark2/Glmark2Activity.java
+++ android/src/org/linaro/glmark2/Glmark2Activity.java
@@ -9,6 +9,10 @@ import android.content.DialogInterface;
 import android.content.Context;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.view.WindowManager;
+import android.view.View;
+import android.view.MotionEvent;
+import android.util.Log;
 
 public class Glmark2Activity extends Activity {
     public static final int DIALOG_EGLCONFIG_FAIL_ID = 0;
@@ -29,9 +33,50 @@ public class Glmark2Activity extends Activity {
         mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, TAG);
         mWakeLock.acquire();
 
+	final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	    | View.SYSTEM_UI_FLAG_FULLSCREEN
+	    | View.SYSTEM_UI_FLAG_IMMERSIVE;
+	getWindow().getDecorView().setSystemUiVisibility(flags);
+	final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+            {
+
+                @Override
+                public void onSystemUiVisibilityChange(int visibility)
+                {
+                    if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                    {
+                        decorView.setSystemUiVisibility(flags);
+                    }
+                }
+            });
+
+
         mGLView = new Glmark2SurfaceView(this);
         setContentView(mGLView);
     }
+
+@Override
+    public boolean onTouchEvent(MotionEvent event) {
+	Log.w("glmark2", "events disscard");
+	return super.onTouchEvent(event);
+    }
+
+    @Override
+public void onWindowFocusChanged(boolean hasFocus)
+{
+    super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+}
 
     @Override
     protected void onPause() {
