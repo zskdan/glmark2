81058a11ac623b6d5ebf2ed2a17375fa51bf9a9c
Zakaria ElQotbi <zakaria.elqotbi@harman.com>
2016-09-20 15:46:25 +0200

    android: add support of arm64bits
    Signed-off-by: Zakaria ElQotbi <zakaria.elqotbi@harman.com>

--- android/jni/Android.ndk.mk
+++ android/jni/Android.ndk.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_MODULE := libglmark2-matrix
-LOCAL_CFLAGS := -DGLMARK2_USE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor \
-                -Wno-error=unused-parameter
+LOCAL_CFLAGS := -DGLMARK2_USE_GLESv2 -Wall -Wextra -Wnon-virtual-dtor \
+                -Wno-error=unused-parameter -fpermissive
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libmatrix/*.cc))
 
@@ -21,11 +21,15 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libglmark2-jpeg
-LOCAL_CFLAGS := -Werror -Wall -Wextra -Wno-error=attributes \
+LOCAL_CFLAGS := -Wall -Wextra -Wno-error=attributes \
+                -fpermissive -DBMP_SUPPORTED -DPPM_SUPPORTED \
+                -DC_ARITH_CODING_SUPPORTED -DD_ARITH_CODING_SUPPORTED \
+                -DMEM_SRCDST_SUPPORTED -DWITH_SIMD -DJPEG_LIB_VERSION=62\
                 -Wno-error=unused-parameter -Wno-error=unused-function -Wno-error=unused-variable
+
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src/libjpeg-turbo/
-LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libjpeg-turbo/simd/*.c)) \
-                   $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libjpeg-turbo/simd/*.S)) \
+LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libjpeg-turbo/simd/jsimd_$(TARGET_ARCH).c)) \
+                   $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libjpeg-turbo/simd/jsimd_$(TARGET_ARCH)_neon.S)) \
                    $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/libjpeg-turbo/*.c))
 
 include $(BUILD_STATIC_LIBRARY)
@@ -34,8 +38,8 @@ include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_MODULE := libglmark2-ideas
-LOCAL_CFLAGS := -DGLMARK_DATA_PATH="" -DGLMARK2_USE_GLESv2 -Werror -Wall -Wextra\
-                -Wnon-virtual-dtor -Wno-error=unused-parameter
+LOCAL_CFLAGS := -DGLMARK_DATA_PATH="" -DGLMARK2_USE_GLESv2 -Wall -Wextra\
+                -Wnon-virtual-dtor -Wno-error=unused-parameter -fpermissive
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src \
                     $(LOCAL_PATH)/src/libmatrix
 LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/scene-ideas/*.cc))
@@ -48,8 +52,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libglmark2-android
 LOCAL_STATIC_LIBRARIES := libglmark2-matrix libglmark2-png libglmark2-ideas libglmark2-jpeg
 LOCAL_CFLAGS := -DGLMARK_DATA_PATH="" -DGLMARK_VERSION="\"2014.03\"" \
-                -DGLMARK2_USE_GLESv2 -Werror -Wall -Wextra -Wnon-virtual-dtor \
-                -Wno-error=unused-parameter
+                -DGLMARK2_USE_GLESv2 -Wall -Wextra -Wnon-virtual-dtor \
+                -Wno-error=unused-parameter -fpermissive
 LOCAL_LDLIBS := -landroid -llog -lGLESv2 -lEGL -lz
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src \
                     $(LOCAL_PATH)/src/libmatrix \
--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,3 +1,4 @@
+APP_ABI := arm64-v8a armeabi
 APP_STL := stlport_static
 APP_PLATFORM := android-9
 APP_BUILD_SCRIPT := jni/Android.ndk.mk
